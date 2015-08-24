af66fa7dd2b78d1837824386a7bb4fd1705a60bd
h.bonjour <h.bonjour@groupeonepoint.com>
2015-08-24 14:06:39 +0200

    Init material ui

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Login from './Login';
 import {RouteHandler, Link} from 'react-router';
+import {AppBar, Styles} from 'material-ui';
+var ThemeManager = Styles.ThemeManager();
 
 class App extends React.Component {
+
+  componentDidMount() {
+    ThemeManager.setTheme(ThemeManager.types.DARK);
+  }
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div class="main-wrapper">
+        <AppBar title="Goplem" />
         <Link to="/">Return to home</Link>
         <Link to="map">Go to Map</Link>
         <RouteHandler/>
@@ -14,5 +27,9 @@ class App extends React.Component {
     );
   }
 }
+
+App.childContextTypes = {
+  muiTheme: React.PropTypes.object
+};
 
 export default App;
